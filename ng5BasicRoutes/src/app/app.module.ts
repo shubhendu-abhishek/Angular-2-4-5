08c16480e101b004992411fa235d49feb85c51bc
Shubhendu Abhishek <shubhendu.abhishek@above-inc.com>
2017-11-26 23:57:22 +0530

    Basic router with ng5

---
+++ ng5BasicRoutes/src/app/app.module.ts
@@ -0,0 +1,46 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { UserComponent } from './user/user.component';
+import { RouterModule, Routes } from '@angular/router';
+
+//Main/Chile Routes Configurations
+const appRoutes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'user', component: UserComponent },
+  {
+    path: 'user', children: [
+      {
+        path: 'list', component: UserListComponent, children: [
+          { path: 'detail/:name', component: UserComponent }
+        ]
+      }
+    ]
+  },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', redirectTo: '/home', pathMatch: 'full' },
+]
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    UserListComponent,
+    UserComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
